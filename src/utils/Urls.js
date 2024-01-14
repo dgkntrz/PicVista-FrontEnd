c6bbd1cdf67838ad6cac020040a9b4fd0a992fbd
turkuazsengul <turkuazsengul@gmail.com>
2024-01-14 18:00:24 +0300

    service base urls changed for container host name

--- src/utils/Urls.js
+++ src/utils/Urls.js
@@ -1,11 +1,11 @@
-export const loginServiceBaseURL = "http://login:8091";
+export const loginServiceBaseURL = "http://4.233.134.59:8091";
 export const loginBasePath = "/login";
 export const signupBasePath = "/signup";
 export const manageUserBasePath = "/update";
 export const updateUserName = "/username";
 export const updatePassword = "/password";
 
-export const imageServiceBaseUrl = "http://images:8092";
+export const imageServiceBaseUrl = "http://4.233.134.59:8092";
 export const imageBasePath = "/images";
 export const addImagePath = "/add";
 export const getImages = "/getAllImages";
